9f69b7c585cbeb239bf0a1b52dd8340ba1b92d8f
Matt Ross <mattross1080@gmail.com>
2019-02-02 15:30:00 -0500

    first set of files for xai ui

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 #----------------------------------------------------------------------------#
 # Imports
 #----------------------------------------------------------------------------#
+import sys
+from flask import Flask, render_template, request, jsonify
+from lifxlan import BLUE, GREEN, LifxLAN, sleep
 
-from flask import Flask, render_template, request
 # from flask.ext.sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 from forms import *
 import os
+import mido
+
+
+outport = mido.open_output('To Live Live')
 
 #----------------------------------------------------------------------------#
 # App Config.
@@ -57,16 +63,39 @@ def login():
     return render_template('forms/login.html', form=form)
 
 
+mood_lookup = {"happy":1,"default":2,"angry":3}
+
 @app.route('/register')
 def register():
-    form = RegisterForm(request.form)
-    return render_template('forms/register.html', form=form)
+    print 'changing'
+    print request.args
+    print request.data
+    print request.values
+    mood = request.args.get("mood","")
+    if mood:
+        control_channel = mood_lookup[mood]
+
+
+    outport.send(mido.Message('control_change', control=control_channel, value=70, channel=2))
+    try:
+        search_lights()
+    except Exception as e:
+        print "problms "
+
+    return jsonify({"hello":"you"})
 
 
 @app.route('/forgot')
 def forgot():
     form = ForgotForm(request.form)
     return render_template('forms/forgot.html', form=form)
+
+
+@app.route('/search')
+def search():
+    form = ForgotForm(request.form)
+    return render_template('forms/forgot.html', form=form)
+
 
 # Error handlers.
 
@@ -90,6 +119,71 @@ if not app.debug:
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
     app.logger.info('errors')
+
+def search_lights():
+    num_lights = None
+    if len(sys.argv) != 2:
+        print("\nDiscovery will go much faster if you provide the number of lights on your LAN:")
+        print("  python {} <number of lights on LAN>\n".format(sys.argv[0]))
+    else:
+        num_lights = int(sys.argv[1])
+
+    # instantiate LifxLAN client, num_lights may be None (unknown).
+    # In fact, you don't need to provide LifxLAN with the number of bulbs at all.
+    # lifx = LifxLAN() works just as well. Knowing the number of bulbs in advance
+    # simply makes initial bulb discovery faster.
+    print("Discovering lights...")
+    lifx = LifxLAN(num_lights)
+
+    # get devices
+    devices = lifx.get_lights()
+    bulb = devices[0]
+    print("Selected {}".format(bulb.get_label()))
+
+    # get original state
+    original_power = bulb.get_power()
+    original_color = bulb.get_color()
+    bulb.set_power("on")
+
+    sleep(0.2) # to look pretty
+
+    print("Toggling power...")
+    toggle_device_power(bulb, 0.2)
+
+    print("Toggling color...")
+    toggle_light_color(bulb, 0.2)
+
+    # restore original color
+    # color can be restored after the power is turned off as well
+    print("Restoring original color and power...")
+    bulb.set_color(original_color)
+
+    sleep(1) # to look pretty.
+
+    # restore original power
+    bulb.set_power(original_power)
+
+def toggle_device_power(device, interval=0.5, num_cycles=3): #TEST
+    original_power_state = device.get_power()
+    device.set_power("off")
+    rapid = True if interval < 1 else False
+    for i in range(num_cycles):
+        device.set_power("on", rapid)
+        sleep(interval)
+        device.set_power("off", rapid)
+        sleep(interval)
+    device.set_power(original_power_state)
+
+def toggle_light_color(light, interval=0.5, num_cycles=3):
+    original_color = light.get_color()
+    rapid = True if interval < 1 else False
+    for i in range(num_cycles):
+        light.set_color(BLUE, rapid=rapid)
+        sleep(interval)
+        light.set_color(GREEN, rapid=rapid)
+        sleep(interval)
+    light.set_color(original_color)
+
 
 #----------------------------------------------------------------------------#
 # Launch.
--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,23 @@
-(function() {
 
-}).call(this);
+function changeLights(mood) {
+
+//   $.ajax({
+//  url: "/register",
+//  context: document.body,
+//   data: {
+//        "VarA": VarA,
+//        "VarB": VarB,
+//        "VarC": VarC
+//    },
+//}).done(function(data) {
+//    console.log(data)
+//  $( this ).addClass( "done" );
+//});
+//
+//
+
+$.get( "/register", { "mood": mood } )
+  .done(function( data ) {
+    alert( "Data Loaded: " + data );
+  });
+}
